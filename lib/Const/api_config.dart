b237c8263cc3eab8909cb9b4032086ab3a6f55c2
skabirgithub <abirshishir1996@gmail.com>
2025-08-01 05:05:43 +0600

    Initial commit

---
+++ lib/Const/api_config.dart
@@ -0,0 +1,6 @@
+class APIConfig {
+  static String domain = 'https://khaata.app/';
+  static String url = '${domain}api/v1';
+  static String registerUrl = '/sign-up';
+  static String businessCategoriesUrl = '/business-categories';
+}
